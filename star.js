bcd68c5f1f18eb31a443efcea9112b216a365835
Shafeeq Danial <sh4feeqerz1726@gmail.com>
2026-09-13 20:09:36 +0800

    made the star system, adding jolt for transients

--- star.js
+++ star.js
@@ -1,17 +1,56 @@
-function Star(angle, orbitRadius, starSize) {
+// Constructor function representing one star
+function Star(angle, orbitRadius, starSize, orbitSpeed) {
 
     // Private properties storing the star's 3D position
     var x = cos(angle) * orbitRadius;
-    var y = random(
-        -orbitRadius * 0.12,
-        orbitRadius * 0.12
-    );
+
+    var y = random(-orbitRadius * 0.12, orbitRadius * 0.12 );
+
     var z = sin(angle) * orbitRadius;
 
     var size = starSize;
+    var speed = orbitSpeed;
+    // Temporary displacement created by a transient
+    var joltX = 0;
+    var joltY = 0;
 
-    // Public method to draw the star
+    /*
+     * Private method that rotates the star around
+     * the centre of the star system.
+     */
+    function update() {
+
+        /*
+         * Store the new coordinates temporarily.
+         * Both calculations must use the old x and z values.
+         */
+        var newX = x * cos(speed) - z * sin(speed);
+
+        var newZ = x * sin(speed) + z * cos(speed);
+
+        x = newX;
+        z = newZ;
+
+        /*
+        * Gradually return the temporary displacement to zero.
+        */
+        joltX *= 0.75;
+        joltY *= 0.75;
+    }
+
+    /*
+    * Public method that gives the star a temporary
+    * random displacement when a transient is detected.
+    */
+    this.jolt = function(strength) {
+        joltX = random(-strength, strength);
+        joltY = random(-strength, strength);
+    };
+
+    // Public method to update and draw the star
     this.draw = function(focalLength) {
+
+        update();
 
         /*
          * Convert the star's 3D position into a position
@@ -20,11 +59,9 @@ function Star(angle, orbitRadius, starSize) {
         var perspective =
             focalLength / (focalLength + z);
 
-        var screenX =
-            width / 2 + x * perspective;
+        var screenX = width / 2 + x * perspective;
 
-        var screenY =
-            height / 2 + y * perspective;
+        var screenY = height / 2 + y * perspective;
 
         // Stars closer to the viewer appear larger
         var displaySize = size * perspective;
@@ -46,6 +83,7 @@ function Star(angle, orbitRadius, starSize) {
 
         noStroke();
         fill(brightness);
+
         ellipse(
             screenX,
             screenY,
@@ -53,3 +91,4 @@ function Star(angle, orbitRadius, starSize) {
         );
     };
 }
+
--- stars.js
+++ stars.js
@@ -7,23 +7,38 @@ function Stars() {
 
     for (var i = 0; i < 180; i++) {
         var angle = random(TWO_PI);
-        var orbitRadius = random(50, min(width, height) * 0.4);
+        var orbitRadius = random(
+            50,
+            min(width, height) * 0.4
+        );
+
         var starSize = random(1.5, 4);
 
+        var orbitSpeed = map(
+            orbitRadius,
+            50,
+            min(width, height) * 0.4,
+            0.012,
+            0.003
+        );
+
         stars.push(
-            new Star(angle, orbitRadius, starSize)
+            new Star(angle, orbitRadius, starSize, orbitSpeed)
         );
     }
 
+    // Public draw method called by the visualisation container
     this.draw = function() {
         push();
 
         background(0);
 
+        // Draw every star in the array
         for (var i = 0; i < stars.length; i++) {
             stars[i].draw(focalLength);
         }
 
+        // Temporary centre point
         noStroke();
         fill(255);
         ellipse(width / 2, height / 2, 20);
@@ -31,3 +46,5 @@ function Stars() {
         pop();
     };
 }
+
+/* End - own code */
